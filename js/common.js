b2bf62bfdf4b3dc141ef8e875b06016ceab3ec2a
Tricky71 <nowwhat@mail.ru>
2021-04-26 13:29:38 +0400

    services slider

--- js/common.js
+++ js/common.js
@@ -5,15 +5,40 @@ $(function() {
 		navbar: {
 			title: '<img src="img/logo-1.svg" alt="Салон красоты Смитлер">',
 		}
-		
-    
-    });
+	});
 
 	var api = $('#my-menu').data('mmenu');
 	api.bind('open:finish', function () {
-		$('.hamburger').addClass('is-active');
-	});
+		$('.hamburger').addClass('is-active'); 
 	api.bind('close:finish', function() {
 		$('.hamburger').removeClass('is-active');
 	});
+
+    })
+
+	$('.owl-carousel--services').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			769: {
+				items: 2
+			},
+			1201: {
+				items: 3
+			}
+		}
+	});
+
+	$('.services__item-info').equalHeights();
+
+	
+	
 })
+
