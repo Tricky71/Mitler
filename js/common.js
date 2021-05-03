a29595d1e83f4f8a14a81daa6e60bfc41efce36b
Tricky71 <nowwhat@mail.ru>
2021-05-03 20:55:20 +0400

    Footer, Up Button, Preloader

--- js/common.js
+++ js/common.js
@@ -65,8 +65,20 @@ $(function() {
 
 		}
 	});
-
+    // Плагин EqualHeights
 	$('.services__item-info').equalHeights();
+    
+	// Кнопка наверх
+	$(window).scroll(function() {
+        if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else {
+			$('.top').removeClass('active');
+		}
+	});
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
 
 	//E-mail Ajax Send
 	$("form.offer").submit(function() { //Change
@@ -85,8 +97,10 @@ $(function() {
 		});
 		return false;
 	});
+});
 
-	
-	
+// Preloader
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
 })
 
